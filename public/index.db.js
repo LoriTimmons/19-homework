d219aa0bce55a7d6e0b8f0f805c4233323ce62ed
Lori Timmons <lorietimmons@gmail.com>
2021-12-19 12:39:29 -0600

    error 3

--- public/index.db.js
+++ public/index.db.js
@@ -1,71 +1,69 @@
-// var to hold db connection 
+// var to hold db connection
 let dataBase;
 
 // Establish a connection to IndexedDB
-const request = indexedDB.open("budget" , 1); 
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-    let db = event.target.result;
-    console.log('This is db', db);
-    db.createObjectStore("budget", {
-        autoIncrement: true
-    })
-}
-request.onsuccess= function (event) {
-    dataBase = event.target.result
-    if(navigator.onLine) {
-        addToDataBase()
-    }
+  let db = event.target.result;
+  console.log("This is db", db);
+  db.createObjectStore('new_budget', {
+    autoIncrement: true,
+  });
+};
+request.onsuccess = function (event) {
+  dataBase = event.target.result;
+  if (navigator.onLine) {
+    addToDataBase();
+  }
 };
 
-request.onerror = function(event) {
-    console.log(event.target.errorCode);
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
 };
 
 // save record
 function saveRecord(record) {
-    const transaction = db.transaction(['budget'], 'readwrite');
-    
-    const budgetObjectStore = transaction.objectStore('budget');
+  const transaction = db.transaction(["new_budget"], "readwrite");
 
-    budgetObjectStore.add(record);
+  const budgetObjectStore = transaction.objectStore("new_budget");
+
+  budgetObjectStore.add(record);
 }
 
 // addToDataBase
 function addToDataBase() {
-    const transaction = dataBase.transaction(['budget'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('budget');
-    const getAll = budgetObjectStore.getAll();
+  const transaction = dataBase.transaction(["new_budget"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_budget");
+  const getAll = budgetObjectStore.getAll();
 
-    getAll.onsuccess = function() {
-        if(getAll.result.length > 0) {
-        fetch ('/api/transaction', {
-            method: 'POST',
-            body: JSON.stringify(getAll.result),
-            headers: {
-            Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(serverResponse => {
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
           if (serverResponse.message) {
             throw new Error(serverResponse);
           }
-          const transaction = db.transaction(['budget'], 'readwrite');
-          const budgetObjectStore = transaction.objectStore('budget');
+          const transaction = db.transaction(["new_budget"], "readwrite");
+          const budgetObjectStore = transaction.objectStore("new_budget");
           budgetObjectStore.clear();
 
-          alert('All saved transactions have been submitted!')
-    })
-    .catch(err => {
-        console.log(err);
+          //   alert('All saved transactions have been submitted!')
+        })
+        .catch((err) => {
+          console.log(err);
         });
     }
-    }
+  };
 }
 
 // listen for app coming back online
-window.addEventListener('online', addToDataBase);
-
-
+window.addEventListener("online", addToDataBase);
