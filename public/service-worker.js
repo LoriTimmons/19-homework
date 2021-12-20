c9100cf8ff233cedb780f25d5714d57aaf03db87
Lori Timmons <lorietimmons@gmail.com>
2021-12-19 21:59:25 -0600

    spelling

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
   "/css/style.css",
   "/js/index.js",
   "index.db.js",
-  "/service-work.js",
+  "/service-worker.js",
   "/icons/icon-72x72.png",
   "/icons/icon-96x96.png",
   "/icons/icon-128x128.png",
